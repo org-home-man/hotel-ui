dec7f76ad3fa43fedcb3d94de84a5882a81666af
chenchao8787 <466027149@qq.com>
2019-03-21 17:42:32 +0800

    ui

---
+++ src/views/Sys/Dept.vue
@@ -0,0 +1,220 @@
+<template>
+  <div class="page-container">
+	<!--工具栏-->
+	<div class="toolbar" style="float:left;padding-top:10px;padding-left:15px;">
+		<el-form :inline="true" :model="filters" :size="size">
+			<el-form-item>
+				<el-input v-model="filters.name" :placeholder="$t('dept.name')"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<kt-button icon="fa fa-search" :label="$t('action.search')" perms="sys:dept:view" type="primary" @click="findTreeData(null)"/>
+			</el-form-item>
+			<el-form-item>
+				<kt-button icon="fa fa-plus" :label="$t('action.add')" perms="sys:dept:add" type="primary" @click="handleAdd"/>
+			</el-form-item>
+		</el-form>
+	</div>
+	<!--表格树内容栏-->
+    <el-table :data="tableTreeDdata" stripe size="mini" style="width: 100%;"
+      v-loading="loading" :element-loading-text="$t('action.loading')">
+      <el-table-column
+        prop="id" header-align="center" align="center" width="80" :label="$t('dept.id')">
+      </el-table-column>
+      <table-tree-column
+        prop="name" header-align="center" treeKey="id" width="150" :label="$t('dept.name')">
+      </table-tree-column>
+      <el-table-column
+        prop="parentName" header-align="center" align="center" width="120" :label="$t('dept.preOrg')">
+      </el-table-column>
+      <el-table-column
+        prop="orderNum" header-align="center" align="center" :label="$t('dept.order')">
+      </el-table-column>
+      <el-table-column
+        prop="createBy" header-align="center" align="center" :label="$t('dept.createName')">
+      </el-table-column>
+      <el-table-column
+        prop="createTime" header-align="center" align="center" :label="$t('dept.createTime')" :formatter="dateFormat">
+      </el-table-column>
+      <el-table-column
+        fixed="right" header-align="center" align="center" width="185" :label="$t('action.operation')">
+        <template slot-scope="scope">
+          <kt-button icon="fa fa-edit" :label="$t('action.edit')" perms="sys:dept:edit" @click="handleEdit(scope.row)"/>
+          <kt-button icon="fa fa-trash" :label="$t('action.delete')" perms="sys:dept:delete" type="danger" @click="handleDelete(scope.row)"/>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- 新增修改界面 -->
+    <el-dialog :title="!dataForm.id ?$t('action.add'):$t('action.edit')" width="40%" :visible.sync="dialogVisible" :close-on-click-modal="false">
+      <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="submitForm()"
+        label-width="100px" :size="size" style="text-align:left;">
+        <el-form-item :label="$t('dept.name')" prop="name">
+          <el-input v-model="dataForm.name" :placeholder="$t('dept.name')"></el-input>
+        </el-form-item>
+        <el-form-item :label="$t('dept.preOrg')" prop="parentName">
+            <popup-tree-input
+              :data="popupTreeData" :props="popupTreeProps" :prop="dataForm.parentName==null?$t('common.topTree'):dataForm.parentName"
+              :nodeKey="''+dataForm.parentId" :currentChangeHandle="handleTreeSelectChange" :placeholder="$t('dept.name')" >
+            </popup-tree-input>
+        </el-form-item>
+        <el-form-item v-if="dataForm.type !== 2" :label="$t('dept.order')" prop="orderNum">
+          <el-input-number v-model="dataForm.orderNum" controls-position="right" :min="0" :label="$t('dept.order')"></el-input-number>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button :size="size"  @click="dialogVisible = false">{{$t('action.cancel')}}</el-button>
+        <el-button :size="size"  type="primary" @click="submitForm()">{{$t('action.confirm')}}</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import KtButton from "@/views/Core/KtButton"
+import TableTreeColumn from '@/views/Core/TableTreeColumn'
+import PopupTreeInput from "@/components/PopupTreeInput"
+import FaIconTooltip from "@/components/FaIconTooltip"
+import { format } from "@/utils/datetime"
+export default {
+	components:{
+    PopupTreeInput,
+    KtButton,
+    TableTreeColumn,
+    FaIconTooltip
+	},
+	data() {
+		return {
+			size: 'small',
+			loading: false,
+			filters: {
+				name: ''
+      },
+      tableTreeDdata: [],
+      dialogVisible: false,
+      dataForm: {
+        id: 0,
+        name: '',
+        parentId: 0,
+        parentName: '',
+        orderNum: 0
+      },
+      dataRule: {
+        name: [
+          { required: true, message: this.$t('action.noNull'), trigger: 'blur' }
+        ],
+        parentName: [
+          { required: true, message: this.$t('action.noNull'), trigger: 'change' }
+        ]
+      },
+      popupTreeData: [],
+      popupTreeProps: {
+				label: 'name',
+				children: 'children'
+			}
+		}
+	},
+	methods: {
+		// 获取数据
+    findTreeData: function () {
+      this.loading = true
+			this.$api.dept.findDeptTree().then((res) => {
+        this.tableTreeDdata = res.data
+        this.popupTreeData = this.getParentMenuTree(res.data)
+        this.loading = false
+			})
+    },
+		// 获取上级机构树
+    getParentMenuTree: function (tableTreeDdata) {
+      let parent = {
+        parentId: 0,
+        name: this.$t('common.topTree'),
+        children: tableTreeDdata
+      }
+      return [parent]
+    },
+		// 显示新增界面
+		handleAdd: function () {
+			this.dialogVisible = true
+			this.dataForm = {
+        id: 0,
+        name: '',
+        parentId: 0,
+        parentName: '',
+        orderNum: 0
+      }
+		},
+		// 显示编辑界面
+		handleEdit: function (row) {
+      this.dialogVisible = true
+      Object.assign(this.dataForm, row);
+		},
+    // 删除
+    handleDelete (row) {
+      this.$confirm(this.$t("action.do"), this.$t('action.tips'), {
+				type: 'warning',
+        cancelButtonText: this.$t('action.cancel'),
+        confirmButtonText: this.$t('action.confirm')
+      }).then(() => {
+        let params = this.getDeleteIds([], row)
+        this.$api.dept.batchDelete(params).then( res => {
+          this.findTreeData()
+          this.$message({message: this.$t('action.delSuccess'), type: 'success'})
+        })
+      })
+    },
+    // 获取删除的包含子机构的id列表
+    getDeleteIds (ids, row) {
+      ids.push({id:row.id})
+      if(row.children != null) {
+        for(let i=0, len=row.children.length; i<len; i++) {
+          this.getDeleteIds(ids, row.children[i])
+        }
+      }
+      return ids
+    },
+      // 机构树选中
+    handleTreeSelectChange (data, node) {
+      this.dataForm.parentId = data.id
+      this.dataForm.parentName = data.name
+    },
+    // 表单提交
+    submitForm () {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+					this.$confirm(this.$t('action.sureSubmit'), this.$t('action.tips'), {}).then(() => {
+						this.dialogVisible = true
+						let params = Object.assign({}, this.dataForm)
+						this.$api.dept.save(params).then((res) => {
+              this.dialogVisible = false
+              if(res.code == 200) {
+								this.$message({ message: this.$t('action.success'), type: 'success' })
+                this.dialogVisible = false
+                this.$refs['dataForm'].resetFields()
+							} else {
+								this.$message({message: this.$t('action.fail'), type: 'error'})
+							}
+							this.findTreeData()
+						})
+					})
+				}
+      })
+    },
+		// 时间格式化
+    dateFormat: function (row, column, cellValue, index){
+      return format(row[column.property])
+    },
+    //取消按钮
+    cancel: function(formName){
+      this.dialogVisible = false;
+      this.$refs[formName].resetFields();
+    }
+
+	},
+	mounted() {
+    this.findTreeData()
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Sys/User.vue
@@ -0,0 +1,277 @@
+<template>
+  <div class="page-container">
+	<!--工具栏-->
+	<div class="toolbar" style="float:left;padding-top:10px;padding-left:15px;">
+		<el-form :inline="true" :model="filters" :size="size">
+			<el-form-item>
+				<el-input v-model="filters.name" :placeholder="$t('user.username')"></el-input>
+			</el-form-item>
+			<el-form-item>
+				<kt-button icon="fa fa-search" :label="$t('action.search')" perms="sys:role:view" type="primary" @click="findPage(null)"/>
+			</el-form-item>
+			<el-form-item>
+				<kt-button icon="fa fa-plus" :label="$t('action.add')" perms="sys:user:add" type="primary" @click="handleAdd" />
+			</el-form-item>
+		</el-form>
+	</div>
+	<div class="toolbar" style="float:right;padding-top:10px;padding-right:15px;">
+		<el-form :inline="true" :size="size">
+			<el-form-item>
+				<el-button-group>
+				<el-tooltip :content="$t('common.refresh')" placement="top">
+					<el-button icon="fa fa-refresh" @click="findPage(null)"></el-button>
+				</el-tooltip>
+				<!--<el-tooltip content="列显示" placement="top">-->
+					<!--<el-button icon="fa fa-filter" @click="displayFilterColumnsDialog"></el-button>-->
+				<!--</el-tooltip>-->
+				<el-tooltip :content="$t('common.export')" placement="top">
+					<el-button icon="fa fa-file-excel-o"></el-button>
+				</el-tooltip>
+				</el-button-group>
+			</el-form-item>
+		</el-form>
+		<!--表格显示列界面-->
+		<table-column-filter-dialog ref="tableColumnFilterDialog" :columns="columns" 
+			@handleFilterColumns="handleFilterColumns"> 
+		</table-column-filter-dialog>
+	</div>
+	<!--表格内容栏-->
+	<kt-table permsEdit="sys:user:edit" permsDelete="sys:user:delete"
+		:data="pageResult" :columns="filterColumns"
+		@findPage="findPage" @handleEdit="handleEdit" @handleDelete="handleDelete">
+	</kt-table>
+	<!--新增编辑界面-->
+	<el-dialog :title="operation?$t('action.add'):$t('action.edit')" width="40%" :visible.sync="dialogVisible" :close-on-click-modal="false">
+		<el-form :model="dataForm" label-width="80px" :rules="dataFormRules" ref="dataForm" :size="size"
+			label-position="right">
+			<el-form-item label="ID" prop="id" v-if="false">
+				<el-input v-model="dataForm.id" :disabled="true" auto-complete="off"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.username')" prop="name">
+				<el-input v-model="dataForm.name" auto-complete="off"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.password')" prop="password">
+				<el-input v-model="dataForm.password" type="password" auto-complete="off"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.org')" prop="deptName">
+				<popup-tree-input 
+					:data="deptData" 
+					:props="deptTreeProps" 
+					:prop="dataForm.deptName" 
+					:nodeKey="''+dataForm.deptId" 
+					:currentChangeHandle="deptTreeCurrentChangeHandle">
+				</popup-tree-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.mail')" prop="email">
+				<el-input v-model="dataForm.email" auto-complete="off"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.phone')" prop="mobile">
+				<el-input v-model="dataForm.mobile" auto-complete="off"></el-input>
+			</el-form-item>
+			<el-form-item :label="$t('user.role')" prop="userRoles" v-if="!operation">
+				<el-select v-model="dataForm.userRoles" multiple :placeholder="$t('action.select')"
+					 style="width: 100%;">
+					<el-option v-for="item in roles" :key="item.id"
+						:label="item.remark" :value="item.id">
+					</el-option>
+				</el-select>
+			</el-form-item>
+		</el-form>
+		<div slot="footer" class="dialog-footer">
+			<el-button :size="size" @click.native="cancel('dataForm')">{{$t('action.cancel')}}</el-button>
+			<el-button :size="size" type="primary" @click.native="submitForm" :loading="editLoading">{{$t('action.submit')}}</el-button>
+		</div>
+	</el-dialog>
+  </div>
+</template>
+
+<script>
+import PopupTreeInput from "@/components/PopupTreeInput"
+import KtTable from "@/views/Core/KtTable"
+import KtButton from "@/views/Core/KtButton"
+import TableColumnFilterDialog from "@/views/Core/TableColumnFilterDialog"
+import { format } from "@/utils/datetime"
+export default {
+	components:{
+		PopupTreeInput,
+		KtTable,
+		KtButton,
+		TableColumnFilterDialog
+	},
+	data() {
+		return {
+			size: 'small',
+			filters: {
+				name: ''
+			},
+			columns: [],
+			filterColumns: [],
+			pageRequest: { pageNum: 1, pageSize: 10 },
+			pageResult: {},
+
+			operation: false, // true:新增, false:编辑
+			dialogVisible: false, // 新增编辑界面是否显示
+			editLoading: false,
+			dataFormRules: {
+				name: [
+					{ required: true, message: this.$t('action.pUsername'), trigger: 'blur' }
+				]
+			},
+			// 新增编辑界面数据
+			dataForm: {
+				id: 0,
+				name: '',
+				password: '123456',
+				deptId: 1,
+				deptName: '',
+				email: '',
+				mobile: '',
+				status: 1,
+				userRoles: []
+			},
+			deptData: [],
+			deptTreeProps: {
+				label: 'name',
+				children: 'children'
+			},
+			roles: []
+		}
+	},
+	methods: {
+		// 获取分页数据
+		findPage: function (data) {
+			if(data !== null) {
+				this.pageRequest = data.pageRequest
+			}
+			this.pageRequest.columnFilters = {name: {name:'name', value:this.filters.name}}
+			this.$api.user.findPage(this.pageRequest).then((res) => {
+				this.pageResult = res.data
+				this.findUserRoles()
+			}).then(data!=null?data.callback:'')
+		},
+		// 加载用户角色信息
+		findUserRoles: function () {
+			this.$api.role.findAll().then((res) => {
+				// 加载角色集合
+				this.roles = res.data	
+			})
+		},
+		// 批量删除
+		handleDelete: function (data) {
+			this.$api.user.batchDelete(data.params).then(data!=null?data.callback:'')
+		},
+		// 显示新增界面
+		handleAdd: function () {
+			this.dialogVisible = true
+			this.operation = true
+			this.dataForm = {
+				id: 0,
+				name: '',
+				password: '',
+				deptId: 1,
+				deptName: '',
+				email: '',
+				mobile: '',
+				status: 1,
+				userRoles: []
+			}
+		},
+		// 显示编辑界面
+		handleEdit: function (params) {
+			this.dialogVisible = true
+			this.operation = false
+			this.dataForm = Object.assign({}, params.row)
+			let userRoles = []
+			for(let i=0,len=params.row.userRoles.length; i<len; i++) {
+				userRoles.push(params.row.userRoles[i].roleId)
+			}
+			this.dataForm.userRoles = userRoles
+		},
+		// 编辑
+		submitForm: function () {
+			this.$refs.dataForm.validate((valid) => {
+				if (valid) {
+					this.$confirm(this.$t('action.sureSubmit'), this.$t('action.Tips'), {}).then(() => {
+						this.editLoading = true
+						let params = Object.assign({}, this.dataForm)
+						let userRoles = []
+						for(let i=0,len=params.userRoles.length; i<len; i++) {
+							let userRole = {
+								userId: params.id,
+								roleId: params.userRoles[i]
+							}
+							userRoles.push(userRole)
+						}
+						params.userRoles = userRoles
+						this.$api.user.save(params).then((res) => {
+							this.editLoading = false
+							if(res.code == 200) {
+								this.$message({ message: this.$t('action.success'), type: 'success' })
+								this.dialogVisible = false
+								this.$refs['dataForm'].resetFields()
+							} else {
+								this.$message({message: this.$t('action.fail'), type: 'error'})
+							}
+							this.findPage(null)
+						})
+					})
+				}
+			})
+		},
+    //取消按钮
+    cancel: function(formName){
+      this.dialogVisible = false;
+      this.$refs[formName].resetFields();
+    },
+		// 获取部门列表
+		findDeptTree: function () {
+			this.$api.dept.findDeptTree().then((res) => {
+				this.deptData = res.data
+			})
+		},
+		// 菜单树选中
+      	deptTreeCurrentChangeHandle (data, node) {
+        	this.dataForm.deptId = data.id
+        	this.dataForm.deptName = data.name
+		},
+		// 时间格式化
+      	dateFormat: function (row, column, cellValue, index){
+          	return format(row[column.property])
+      	},
+		// 处理表格列过滤显示
+      	displayFilterColumnsDialog: function () {
+			this.$refs.tableColumnFilterDialog.setDialogVisible(true)
+      	},
+		// 处理表格列过滤显示
+      	handleFilterColumns: function (data) {
+			this.filterColumns = data.filterColumns
+			this.$refs.tableColumnFilterDialog.setDialogVisible(false)
+      	},
+		// 处理表格列过滤显示
+      	initColumns: function () {
+			this.columns = [
+				{prop:"id", label:"id", minWidth:50},
+				{prop:"name", label:'username', minWidth:120},
+				{prop:"deptName", label:'org', minWidth:120},
+				{prop:"roleNames", label:'role', minWidth:100},
+				{prop:"email", label:'mail', minWidth:120},
+				{prop:"mobile", label:'phone', minWidth:100},
+        {prop:"status", label:'state', minWidth:70},
+				// {prop:"createBy", label:"创建人", minWidth:120},
+				// {prop:"createTime", label:"创建时间", minWidth:120, formatter:this.dateFormat}
+				// {prop:"lastUpdateBy", label:"更新人", minWidth:100},
+				// {prop:"lastUpdateTime", label:"更新时间", minWidth:120, formatter:this.dateFormat}
+			]
+			this.filterColumns = JSON.parse(JSON.stringify(this.columns));
+      	}
+	},
+	mounted() {
+		this.findDeptTree()
+		this.initColumns()
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
