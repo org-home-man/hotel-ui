c54b698e10c4b813c4af28f9cd829354c569034f
zcl <15002808281@163.com>
2019-03-26 23:34:36 +0800

    语言切换，暂时修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,81 +1,95 @@
-<template>
+<template xmlns:v-popover="">
   <el-form :model="loginForm" :rules="fieldRules" ref="loginForm" label-position="left" label-width="0px" class="demo-ruleForm login-container">
     <span class="tool-bar">
       <!-- 主题切换 -->
-      <theme-picker style="float:right;" class="theme-picker" :default="themeColor" @onThemeChange="onThemeChange"></theme-picker>
+      <!--<theme-picker style="float:right;" class="theme-picker" :default="themeColor" @onThemeChange="onThemeChange"></theme-picker>-->
       <!-- 语言切换 -->
-      <!-- <lang-selector class="lang-selector"></lang-selector>    -->
+      <el-menu class="el-menu-demo" :background-color="themeColor" :text-color="themeColor" :active-text-color="themeColor" mode="horizontal">
+        <el-menu-item index="1" v-popover:popover-lang>
+            <!-- 语言切换 -->
+            <li style="color:#fff;" class="fa fa-language fa-lg"></li>
+            <el-popover ref="popover-lang" placement="bottom-start" trigger="click" v-model="langVisible">
+              <div class="lang-item" @click="changeLanguage('zh_cn')">简体中文</div>
+              <div class="lang-item" @click="changeLanguage('en_us')">English</div>
+            </el-popover>
+          </el-menu-item>
+      </el-menu>
+      <!--<lang-selector class="lang-selector"></lang-selector>-->
     </span>
-    <h2 class="title" style="padding-left:22px;" >系统登录</h2>
+    <h2 class="title" style="padding-left:22px;" ></h2>
     <el-form-item prop="account">
-      <el-input type="text" v-model="loginForm.account" auto-complete="off" placeholder="账号"></el-input>
+      <el-input type="text" v-model="loginForm.account" auto-complete="off" :placeholder="$t('common.username')"></el-input>
     </el-form-item>
     <el-form-item prop="password">
-      <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+      <el-input type="password" v-model="loginForm.password" auto-complete="off" :placeholder="$t('common.passwd')"></el-input>
     </el-form-item>
     <el-form-item >
       <el-col :span="12">
         <el-form-item prop="captcha">
-          <el-input type="test" v-model="loginForm.captcha" auto-complete="off" placeholder="验证码, 单击图片刷新"
-            style="width: 100%;">
+          <el-input type="test" v-model="loginForm.captcha" auto-complete="off" :placeholder="$t('common.validmsg')"
+                    style="width: 100%;">
           </el-input>
         </el-form-item>
       </el-col>
       <el-col class="line" :span="1">&nbsp;</el-col>
       <el-col :span="11">
         <el-form-item>
-            <img style="width: 100%;" class="pointer" :src="loginForm.src" @click="refreshCaptcha">
+          <img style="width: 100%;" class="pointer" :src="loginForm.src" @click="refreshCaptcha">
         </el-form-item>
       </el-col>
     </el-form-item>
     <!-- <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox> -->
     <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:48%;" @click.native.prevent="reset">重 置</el-button>
-      <el-button type="primary" style="width:48%;" @click.native.prevent="login" :loading="loading">登 录</el-button>
+      <el-button type="primary" style="width:48%;" @click.native.prevent="reset">{{$t('common.reset')}}</el-button>
+      <el-button type="primary" style="width:48%;" @click.native.prevent="login" :loading="loading">{{$t('common.login')}}</el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import Cookies from "js-cookie"
-import ThemePicker from "@/components/ThemePicker"
-import LangSelector from "@/components/LangSelector"
-export default {
-  name: 'Login',
-  components:{
-    ThemePicker,
-    LangSelector
-  },
-  data() {
-    return {
-      loading: false,
-      loginForm: {
-        account: 'admin',
-        password: 'admin',
-        captcha:'',
-        src: ''
-      },
-      fieldRules: {
-        account: [
-          { required: true, message: '请输入账号', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '请输入密码', trigger: 'blur' }
-        ]
-        // ,
-        // captcha: [
-        //   { required: true, message: '请输入验证码', trigger: 'blur' }
-        // ]
-      },
-      checked: true
-    }
-  },
-  methods: {
-    login() {
-      this.loading = true
-      let userInfo = {account:this.loginForm.account, password:this.loginForm.password, captcha:this.loginForm.captcha}
-      this.$api.login.login(userInfo).then((res) => {
+  import { mapState } from 'vuex'
+  import Cookies from "js-cookie"
+  import ThemePicker from "@/components/ThemePicker"
+  import LangSelector from "@/components/LangSelector"
+  export default {
+    name: 'Login',
+    components:{
+      ThemePicker,
+      LangSelector
+    },
+    data() {
+      return {
+        loading: false,
+        loginForm: {
+          account: 'admin',
+          password: 'admin',
+          captcha:'',
+          src: ''
+        },
+        fieldRules: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
+          ]
+          // ,
+          // captcha: [
+          //   { required: true, message: '请输入验证码', trigger: 'blur' }
+          // ]
+        },
+        checked: true,
+        langVisible: false
+      }
+    },
+    methods: {
+      login() {
+        this.loading = true
+        let userInfo = {account:this.loginForm.account, password:this.loginForm.password, captcha:this.loginForm.captcha}
+        console.log(this.$api);
+        console.log(this.$api.login);
+        this.$api.login.login(userInfo).then((res) => {
+          console.log("res:"+res.toString());
           if(res.msg != null) {
             this.$message({
               message: res.msg,
@@ -84,37 +98,44 @@ export default {
           } else {
             Cookies.set('token', res.data.token) // 放置token到Cookie
             sessionStorage.setItem('user', userInfo.account) // 保存用户到本地会话
-            this.$store.commit('menuRouteLoaded', false) // 要求重新加载导航菜单
+            this.$store.commit('menuRouteLoaded', true) // 要求重新加载导航菜单
             this.$router.push('/')  // 登录成功，跳转到主页
           }
           this.loading = false
         }).catch((res) => {
+          console.log("错误："+ res.message );
           this.$message({
-          message: res.message,
-          type: 'error'
+            message: res.message,
+            type: 'error'
           })
         });
+      },
+      refreshCaptcha: function(){
+        this.loginForm.src = this.global.baseUrl + "/captcha.jpg?t=" + new Date().getTime();
+      },
+      reset() {
+        this.$refs.loginForm.resetFields()
+      },
+      // 切换主题
+      onThemeChange: function(themeColor) {
+        this.$store.commit('setThemeColor', themeColor)
+      },
+      // 语言切换
+      changeLanguage(lang) {
+        lang === '' ? 'zh_cn' : lang
+        this.$i18n.locale = lang
+        this.langVisible = false
+      }
     },
-    refreshCaptcha: function(){
-      this.loginForm.src = this.global.baseUrl + "/captcha.jpg?t=" + new Date().getTime();
+    mounted() {
+      this.refreshCaptcha()
     },
-    reset() {
-      this.$refs.loginForm.resetFields()
-    },
-    // 切换主题
-    onThemeChange: function(themeColor) {
-      this.$store.commit('setThemeColor', themeColor)
+    computed:{
+      ...mapState({
+        themeColor: state=>state.app.themeColor
+      })
     }
-  },
-  mounted() {
-    this.refreshCaptcha()
-  },
-  computed:{
-    ...mapState({
-      themeColor: state=>state.app.themeColor
-    })
   }
-}
 </script>
 
 <style lang="scss" scoped>
