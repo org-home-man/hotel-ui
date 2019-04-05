3ad68956db3a78a0ce5b28338cbaa96a13f0bf93
zcl <15002808281@163.com>
2019-04-05 18:00:13 +0800

    房屋信息

---
+++ src/http/moudules/sysParaConfig.js
@@ -0,0 +1,48 @@
+import axios from '../axios'
+
+/* 
+ * 用户管理模块
+ */
+
+// 保存
+export const save = (data) => {
+    return axios({
+        url: '/sysParaConfig/save',
+        method: 'post',
+        data
+    })
+}
+// 删除
+export const batchDelete = (data) => {
+    return axios({
+        url: '/sysParaConfig/delete',
+        method: 'post',
+        data
+    })
+}
+// 分页查询
+export const findPage = (data) => {
+    return axios({
+        url: '/sysParaConfig/findPage',
+        method: 'post',
+        data
+    })
+}
+// 查找用户的菜单权限标识集合
+export const findPermissions = (params) => {
+    return axios({
+        url: '/sysParaConfig/findPermissions',
+        method: 'get',
+        params
+    })
+}
+
+// 数据字典查询
+export const findKeyValue = (data) => {
+  return axios({
+    url: '/sysParaConfig/findListData',
+    method: 'post',
+    data
+  })
+}
+
