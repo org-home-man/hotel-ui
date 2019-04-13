842a359e41dc9a16dd592a371c43c516fe6b29c9
zcl <15002808281@163.com>
2019-04-13 14:54:29 +0800

    权限组件增加

---
+++ src/views/Core/KtInput.vue
@@ -0,0 +1,38 @@
+<template>
+
+  <el-input v-model="model" :disabled="!hasPerms(perms)"></el-input>
+
+</template>
+
+<script>
+import { hasPermission } from '@/permission/index.js'
+export default {
+  name: 'KtInput',
+  props: {
+    perms: {  // 按钮权限标识，外部使用者传入
+      type: String,
+      default: null
+    },
+    model:{
+      type:String,
+      default:null
+    }
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+    hasPerms: function (perms) {
+      // 根据权限标识和外部指示状态进行权限判断
+      return hasPermission(perms) & !this.disabled
+    }
+  },
+  mounted() {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
