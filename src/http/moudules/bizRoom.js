4f616f899325461c80dabb032c5906b90e679a3b
zcl <15002808281@163.com>
2019-04-26 14:32:07 +0800

    客房信息修改

--- src/http/moudules/bizRoom.js
+++ src/http/moudules/bizRoom.js
@@ -40,8 +40,11 @@ export const batchDelete = (data) => {
 // 分页查询
 export const findPage = (data) => {
     return axios({
-        url: '/bizRoom/findPage',
+        url: '/bizRoom/page',
         method: 'post',
+        headers:{
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
         data
     })
 }
@@ -58,9 +61,24 @@ export const uploadFile = (data) => {
   return axios({
     url: '/bizRoom/uploadFile',
     method: 'post',
-    headers: {
-      'Content-Type': 'multipart/form-data;boundary=---12321 '
-    },
+    data
+  })
+}
+
+//生成日期数据，并且根据用户生成数据是否有设置相应数据需要替换。
+export const producePriceCalendar = (data) => {
+  return axios({
+    url: '/bizRoom/producePriceCalendar',
+    method: 'post',
+    data
+  })
+}
+
+//用户输入生成数据组，组装数据组，为了客户可以重复输入
+export const priceDatePro = (data) => {
+  return axios({
+    url: '/bizRoom/priceDatePro',
+    method: 'post',
     data
   })
 }
